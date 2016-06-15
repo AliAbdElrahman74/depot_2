93fc2e7998738fe9a19f4b0e5b8772b1e9762a1c
AliAbdElrahman74 <aliabdelrahmanweka74@gmail.com>
2016-06-15 05:08:20 +0200

    Display the catalog list

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,14 @@
 class Product < ActiveRecord::Base
 
+	#default_scope { where(order: 'title') }
 	validates  :title, :description, :image_url, :presence => true
 	validates  :price, :numericality => {:greater_than_or_equal_to => 0.01}
 	validates  :title, :uniqueness => true
+	#validates  :title, :length => {:greater_than_or_equal_to => 10}
 	validates  :image_url, :format => {
 		:with => %r{\.(gif|jpg|png)$}i,
 		:message => 'must be a URL for GIF, JPG or PNG image.',
 		:multiline => true
 	}
+
 end
