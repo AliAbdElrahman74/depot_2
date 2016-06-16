3521714ae3406ba333a1f138c63b4d68b2242ab0
AliAbdElrahman74 <aliabdelrahmanweka74@gmail.com>
2016-06-16 08:43:51 +0200

    Ajax to the cart

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( depot.css )
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( jquery_ujs.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
